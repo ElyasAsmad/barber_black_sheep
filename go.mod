222d64c87cdcfa57a7219d747d5000287549bd35
PlashSpeed-Aiman <maiman.ar@live.iium.edu.my>
2023-11-08 12:37:23 +0800

    cors update

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.21
 require github.com/go-chi/chi/v5 v5.0.10
 
 require (
+	github.com/go-chi/cors v1.2.1
 	github.com/go-chi/jwtauth/v5 v5.1.1
 	github.com/golang-jwt/jwt/v5 v5.0.0
+	github.com/lestrrat-go/jwx/v2 v2.0.11
 	github.com/mattn/go-sqlite3 v1.14.17
 	golang.org/x/crypto v0.14.0
 )
@@ -18,7 +20,6 @@ require (
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
 	github.com/lestrrat-go/httprc v1.0.4 // indirect
 	github.com/lestrrat-go/iter v1.0.2 // indirect
-	github.com/lestrrat-go/jwx/v2 v2.0.11 // indirect
 	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	_ "github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/jwtauth/v5"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
@@ -26,6 +27,16 @@ func main() {
 	log.Default().Println("Server started")
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Mount("/api/v1/business/owner_services", business_service.MakeHTTPHandler())
 	r.Mount("/api/v1/business/appointments", business_appointment.MakeHttpHandler())
 	r.Group(func(r chi.Router) {
